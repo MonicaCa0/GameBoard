98080bfa54fa4b0e8a96872e866ba38463bc297c
Monica <monicacao321@gmail.com>
2023-02-02 23:11:19 -0500

    Game

--- src/Dice.java
+++ src/Dice.java
@@ -2,9 +2,6 @@ import java.util.Random;
 
 public class Dice {
 
-    private final int NUMBER_OF_TURNS = 5;
-    private final int SPACES_TO_WIN = 20;
-
 
     public int randomNumber() {
         Random random = new Random();
@@ -13,12 +10,17 @@ public class Dice {
 
     public void turns(){
         int total = 0;
+        int NUMBER_OF_TURNS = 5;
+        int SPACES_TO_WIN = 20;
+
         for(int i = 1; i <= NUMBER_OF_TURNS; i++) {
             System.out.println("#" + i + " " + "You Rolled a" + " " + randomNumber());
             total += randomNumber();
         }
             System.out.println("Your total is " + total +"!");
-            if(total == SPACES_TO_WIN){
+
+
+        if(total == SPACES_TO_WIN){
                 System.out.println("YOU WIN!");
             } else{
                 System.out.println("You Lose:(");
